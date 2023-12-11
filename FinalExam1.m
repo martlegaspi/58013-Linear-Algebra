105181af20a4af7c460a757a028074a5f25a17e4
martlegaspi <98940218+martlegaspi@users.noreply.github.com>
2023-12-11 08:00:32 +0800

    Add files via upload

---
+++ FinalExam1.m
@@ -0,0 +1,10 @@
+%Question 1: Student A, Student B, and Student C have a total of Php 89 cash in their banks. Student A has 6 less than Student C. Student B has 3
+%times what Student C has. How much does each student have? **Solve using MATLAB Codes (40 points)
+
+A = 89;
+B = 89;
+C = 89;
+
+StudentA = C - 6
+StudentB = 3*C
+StudentC = C
